b594e277dd99136e660b184e0e3dc5feabf5bea0
youmnaMoatazdwidar <yomna.mo.ms1@alexu.edu.eg>
2020-04-07 23:13:35 +0200

    milestone 1 completed

---
+++ library/app/Http/Controllers/UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Intervention\Image\Facades\Image;
+
+class UserController extends Controller
+{
+    //
+    public function show()
+    {
+        $user = Auth::user();
+        $posts = $user->posts()->latest()->get();
+        return view('users.profile', [
+            'user' => $user,
+            'posts' => $posts,
+        ]);
+    }
+
+    public function update_avatar(Request $request)
+    {
+
+        $user = Auth::user();
+        if ($request->hasFile('profile_img')) {
+            $avatar = $request->file('profile_img');
+            $filename = time() . '.' . $avatar->getClientOriginalExtension();
+            Image::make($avatar)->resize(300, 300)->save(public_path('/uploads/avatars/' . $filename));
+
+            $user->profile_img = $filename;
+            $user->save();
+
+        }
+        $posts = $user->posts()->latest()->get();
+        return view('users.profile', [
+            'user' => $user,
+            'posts' => $posts,
+        ]);
+    }
+
+
+}
